fb5e4017be8021eaae310a7e517bf6eec0a2542c
Oleksii Tishonko <OleksiiTishonko@gmail.com>
2023-11-07 12:50:34 -0700

    add routes for products
    add:
    *create product
    *get product by id
    *update product by id
    *delete product by id

--- Server/controllers/productController.js
+++ Server/controllers/productController.js
@@ -13,3 +13,79 @@ exports.getAllProducts = async (req, res) =>{
         },
     });
 }
+
+exports.getProduct = async (req, res) => {
+    try{
+        const product = await Product.findById(req.params.id);
+
+        res.status(200).json({
+            status: 'success',
+            data:{
+                product,
+            },
+        })
+    }
+    catch(err){
+        res.status(404).json({
+            status: 'failed to get a product',
+            message: err,
+        });
+    }
+}
+
+exports.updateProduct = async (req, res)=>{
+    console.log("Updated tour");
+    try{
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                product: product,
+            },
+        });
+    }
+    catch(err){
+        res.status(404).json({
+            status: 'failed to update product',
+            message: err,
+        });
+    }
+}
+exports.deleteProduct = async (req, res)=>{
+    try{
+        await Product.findByIdAndDelete(req.params.id);
+
+        res.status(204).json({
+            status: 'success',
+            data: null,
+        });
+    }
+    catch(err){
+        res.status(404).json({
+            status: 'failed to delete product',
+            message: err,
+        });
+    }
+}
+exports.createProduct = async (req, res) => {
+    try{
+        const newProduct = await Product.create(req.body);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                product: newProduct,
+            },
+        });
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).json({
+            status: 'failed to create product (invalid product data)',
+            message: err,
+        });
+    }
+}
--- Server/routes/productRoutes.js
+++ Server/routes/productRoutes.js
@@ -5,9 +5,14 @@ const router = express.Router();
 
 router
     .route('/')
-    .get(productsController.getAllProducts);
+    .get(productsController.getAllProducts)
+    .post(productsController.createProduct);
+    
 
-
+router.route('/:id')
+    .get(productsController.getProduct)
+    .patch(productsController.updateProduct)
+    .delete(productsController.deleteProduct);
 
 
 
